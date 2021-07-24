c5e7d8d1267e69025424a0b6100bcd4fca55b8ff
Saksit.Sri <saksit.sri@ntb.co.th>
2021-07-24 12:31:41 +0700

    Test

--- solid-workshop/src/test/java/dip/GenerateIdServiceTest.java
+++ solid-workshop/src/test/java/dip/GenerateIdServiceTest.java
@@ -3,14 +3,26 @@ package dip;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
 
-class GenerateIdServiceTest {
+import static org.junit.Assert.*;
+class Random7 extends Random {
+    @Override
+    public int nextInt(int bound) {
+        return 7;
+    }
+
+}
+
+public class GenerateIdServiceTest {
+
     @Test
     @DisplayName("ต้องได้ id=XYZ7")
     public void case01() {
         GenerateIdService service = new GenerateIdService();
+        service.setRandom(new Random7());
         String id = service.getId();
         assertEquals("XYZ7", id);
     }
+
 }
